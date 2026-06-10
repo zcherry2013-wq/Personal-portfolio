9699a8e75eaebb8fdf4cab0c764a79baade38e16
zcherry2013-wq <zcherry2013@gmail.com>
2026-06-10 16:23:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
+document.getElementById("contactForm").addEventListener("submit", function(event){
 
     event.preventDefault();
 
-    alert("Message submitted successfully!");
+    alert("Thank you for your message! I will get back to you soon.");
+
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        document.querySelector(
+            this.getAttribute("href")
+        ).scrollIntoView({
+            behavior:"smooth"
+        });
+
+    });
 
 });
